abde8461ee3d9d35065a517066724871496bd37a
Ryan Vennell <ryan.vennell@gmail.com>
2017-10-19 15:43:17 -0500

    Final tweaks to fundraiser trigger

--- triggers/fundraiser.js
+++ triggers/fundraiser.js
@@ -1,10 +1,30 @@
 // triggers on transaction with a certain tag
-const triggerActivity = (z, bundle) => {
+const triggerFundraiser = (z, bundle) => {
   const responsePromise = z.request({
     url: 'https://api.classy.org/2.0/organizations/{{bundle.inputData.organization_id}}/campaigns'
-  });
-  return responsePromise
-    .then(response => JSON.parse(response.content).data);
+  })
+
+  return responsePromise.then((response) => {
+    var campaigns = JSON.parse(response.content).data;
+    var fundraiserPromises = [];
+
+    for (var i = 0; i < campaigns.length; i++) {
+      fundraiserPromises.push(z.request({
+        url: 'https://api.classy.org/2.0/campaigns/' + campaigns[i].id + '/fundraising-teams?with=team_lead&per_page=100&sort=updated_at:desc'
+      }));
+    }
+
+    return Promise.all(fundraiserPromises).then(responses => {
+      var fundraisers = [];
+
+      for (var i = 0; i < responses.length; i++) {
+        var fr = JSON.parse(responses[i].content).data;
+        fundraisers = fundraisers.concat(fr);
+      }
+
+      return fundraisers;
+    })
+  });;
 };
 
 module.exports = {
@@ -21,6 +41,6 @@ module.exports = {
       inputFields: [
           {key: 'organization_id', type: 'number',  helpText: 'Which organization to check for fundraisers on.'}
       ],
-      perform: triggerActivity
+      perform: triggerFundraiser
   }
 };
